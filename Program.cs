6c986e9c8da5f5cf972bcdec14e2cec220ac18b6
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-30 20:06:01 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace How_to_Square_from_a_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqureDigits.SquareAllDigits(1);
+        }
+    }
+}
---
+++ SqureDigits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How_to_Square_from_a_string
+{
+    public  class SqureDigits
+    {
+        public static int SquareAllDigits(int n)
+        {
+            string output = "";
+            foreach (char c in n.ToString())
+            {
+                int square = int.Parse(c.ToString());
+                output += (square * square);
+                Console.WriteLine(int.Parse(output));
+            }
+            return int.Parse(output);
+        }
+    }
+}
